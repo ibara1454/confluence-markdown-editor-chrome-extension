6f50d54eaf02638643aa2a219b5616395489615f
Ibara <ibara1454@gmail.com>
2020-03-11 03:40:58 +0900

    docs(v-style): add component document

--- src/basics/VStyle.vue
+++ src/basics/VStyle.vue
@@ -1,6 +1,13 @@
 <script lang="ts">
 import Vue from 'vue';
 
+/**
+ * Same as native style tags.
+ * This class is one of the workarounds for that you can't use native style
+ * tags in template. To provide the same effect as native script,
+ * you could use style via components.
+ * @displayName VStyle
+ */
 export default Vue.extend({
   name: 'VStyle',
 
