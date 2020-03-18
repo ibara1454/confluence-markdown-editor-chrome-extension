48b884eba2110d7aba4a57d84a50a9deb237313d
Ibara <ibara1454@gmail.com>
2020-03-19 02:53:08 +0900

    fix: put markdown editor into context

--- src/pages/editor/App.vue
+++ src/pages/editor/App.vue
@@ -1,21 +1,31 @@
 <template>
-  <markdown-editor id="editor" v-model="computedText" />
+  <context class="frame">
+    <!-- Load stylesheet again, since outside style cannot effect on inner frame -->
+    <link type="text/css" rel="stylesheet" :href="computedUrl">
+    <markdown-editor class="editor" v-model="computedText" />
+  </context>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Context from '@/components/Context.vue';
 import MarkdownEditor from '@/components/MarkdownEditor.vue';
+import { getExternalUrl } from '@/utils/extension';
 
 export default Vue.extend({
   name: 'App',
 
-  components: { MarkdownEditor },
+  components: { Context, MarkdownEditor },
 
   props: {
     text: { type: String, required: true },
   },
 
   computed: {
+    computedUrl(): string {
+      return getExternalUrl('app.css');
+    },
+
     computedText: {
       get(): string { return this.text; },
 
@@ -27,8 +37,17 @@ export default Vue.extend({
 });
 </script>
 
-<style>
-#editor {
+<style scoped>
+.frame {
+  position: absolute;
+  top: 60px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: calc(100% - 60px);
+}
+
+.editor {
   height: 100%;
   width: 100%;
 }
