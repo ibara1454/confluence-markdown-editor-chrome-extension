dba472c5d147caa0d75b05b532f5c5525d52e797
Ibara <ibara1454@gmail.com>
2020-03-20 17:23:27 +0900

    refactor: remove unused message type

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,8 @@
-import { Message } from '@/model/types';
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // read changeInfo data and do something with it
   // like send the new url to content scripts
   if (changeInfo.url) {
-    const message: Message = {
-      event: 'PageChanged',
-    };
-    chrome.tabs.sendMessage(tabId, message);
+    chrome.tabs.sendMessage(tabId, null);
   }
 });
