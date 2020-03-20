c866b65b691fa77a87b52e4da647d1a617466f43
Ibara <ibara1454@gmail.com>
2020-03-20 20:56:53 +0900

    refactor: build all sources into dist directory

--- src/utils/extension.ts
+++ src/utils/extension.ts
@@ -1,10 +1,6 @@
-import path from 'path';
-
 /**
  * Returns the absolute url of given url in extension.
  * @param url relative path
  */
 // eslint-disable-next-line import/prefer-default-export
-export function getExternalUrl(url: string): string {
-  return chrome.runtime.getURL(path.resolve('dist', url));
-}
+export const getExternalUrl: (path: string) => string = chrome.runtime.getURL;
