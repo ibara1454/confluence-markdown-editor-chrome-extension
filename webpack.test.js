70bd807616ad3a292e437d83cdcedf5f4a7ea253
Ibara <ibara1454@users.noreply.github.com>
2020-03-29 21:08:08 +0900

    fix: failure of test. rewrite merge process by webpack-merge (#9)

--- webpack.test.js
+++ webpack.test.js
@@ -2,22 +2,33 @@ const nodeExternals = require('webpack-node-externals');
 const merge = require('webpack-merge');
 const dev = require('./webpack.dev');
 
-// Override the config of vue-loader
-// The default value of this option is `true` when `target: node`,
-// and will cause errors like
-// ```
-// [Vue warn]: Property or method "_ssrNode" is not defined on the instance but referenced during render. Make sure to declare reactive data properties in the data option.
-// ```
-// The workaround is setting `optimizeSSR` to false.
-// https://github.com/vuejs/vue-loader/issues/885#issuecomment-375802186
-// https://vue-loader-v14.vuejs.org/ja/options.html#optimizessr
-dev.module.rules[0].options.optimizeSSR = false;
-
-module.exports = merge(dev, {
+// Using smart merging to merge same loaders merge into a single loader value
+// https://github.com/survivejs/webpack-merge#smart-merging
+module.exports = merge.smart(dev, {
   // In order to use JSDOM for testing dom's modifications,
   // `target` should be set to `node`.
   target: 'node',
 
+  // Override the config of vue-loader
+  // The default value of this option is `true` when `target: node`,
+  // and will cause errors like
+  // ```
+  // [Vue warn]: Property or method "_ssrNode" is not defined on the instance but referenced during render. Make sure to declare reactive data properties in the data option.
+  // ```
+  // The workaround is setting `optimizeSSR` to false.
+  // https://github.com/vuejs/vue-loader/issues/885#issuecomment-375802186
+  // https://vue-loader-v14.vuejs.org/ja/options.html#optimizessr
+  module:{
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          optimizeSSR: false,
+        }
+      },
+    ],
+  },
   // Using mocha-webpack with jsdom
   // https://github.com/zinserjan/mocha-webpack/blob/e2aeeb0dc460f09b77808dcc45b595aa54a3fdcd/docs/guides/jsdom.md
   externals: [
